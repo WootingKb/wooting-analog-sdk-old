7684dc66de6285924a4ed597df9b5da18191bc3e
Jeroen <jeroen@wooting.nl>
2018-07-03 16:45:41 +0200

    Add solution. Change comments to to reflect row/column index ing

--- src/wooting-analog-sdk.h
+++ src/wooting-analog-sdk.h
@@ -17,9 +17,8 @@ typedef void(*void_cb)(void);
 
 /** @brief Check if keyboard connected.
 
-This function offers a quick check if the keyboard is connected. This doesn't open the keyboard or influences reading.
-
-It is recommended to poll this function at the start of your application before starting reading.
+This function offers a check if the keyboard is connected. 
+The read functions will automatically call this function on first use.
 
 @ingroup API
 
@@ -30,7 +29,7 @@ WOOTINGANALOGSDK_API bool wooting_kbd_connected(void);
 
 /** @brief Set callback for when a keyboard disconnect.
 
-The callback will be called when a Wooting keyboard disconnects. Right now this will only trigger after a failed read.
+The callback will be called when a Wooting keyboard disconnects. It will only trigger after a failed read.
 
 @ingroup API
 
@@ -41,13 +40,15 @@ WOOTINGANALOGSDK_API void wooting_set_disconnected_cb(void_cb cb);
 
 /** @brief Get the analog value of a single key.
 
-This function returns an analog value of a single key. A SCAN_CODES enumaration is used as argument. These can be found
-in ScanCodes.h or on the Wooting developer portal. Scan codes are used to prevent ambiguity between keyboard layouts.
+This function returns an analog value of a single key. The arguments are a row and column index of the keyboard array.
+The keyboard matrix with row columns indexing can be found in the resources folder.
 
-It is not necesarry to initialize the keyboard before reading, but if the keyboard is not connected this function will return 0.
+It is not necesarry to initialize the keyboard before reading. If the keyboard is not connected this function will return 0.
+Indexes out of bound will also return 0.
 
 @ingroup API
-@param key Scan code of the key that will be read
+@param row Row index of the key that will be read
+@param column Column index of the key that will be read
 
 @returns
 This function returns the analog value of the key from 0 - 255
@@ -56,14 +57,15 @@ WOOTINGANALOGSDK_API uint8_t wooting_read_analog(uint8_t row, uint8_t column);
 
 /** @brief Get the full analog buffer.
 
-This function can be used to get a buffer of all the keys that are pressed up to a maximum of 16 keys. This can be used for easier access
-to all the keys that are currently pressed. The data is ordered as [scan_code0, analog_value0, scan_code1, analog_value1, .... , scan_code15, analog_value15] 
+This function can be used to get a buffer of all the keys that are pressed up to a maximum of 16 keys. The maximum size of the buffer is 32 bytes (2 bytes * 16 keys)
+The data is ordered as [scan_code0, analog_value0, scan_code1, analog_value1, .... , scan_code15, analog_value15] 
+The keyboard matrix with scan code indexing can be found in the resources folder.
 
 It is not necesarry to initialize the keyboard before reading. If the keyboard is not connected this function will return -1.
 
 @ingroup API
-@param data A buffer to put the read data into.
-@param items Length of the data buffer
+@param data A buffer to put the read data into (2 - 32 bytes).
+@param items Length of the data buffer in bytes
 
 @returns
 This function returns items written and -1 on error.
